07f1b364a4aa8d5b15fcebb1c1ba8331e8af7499
Leptar <leo.ultimate34@gmail.com>
2025-01-28 10:26:27 -0500

    add main : add commands for tools

--- ToolUE_RCP/ToolUE_RCP/ToolUE_RCP.cpp
+++ ToolUE_RCP/ToolUE_RCP/ToolUE_RCP.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include "json.hpp"
 using json = nlohmann::json;
 
-string HowToUse =
+string HOWTOUSE =
     "How to use ToolUE_RCP :\n"
     "ToolUE_RCP [CHEMIN DU UPROJECT] show-infos\n"
     "ToolUE_RCP [CHEMIN DU UPROJECT] build\n"
@@ -13,33 +13,38 @@ string HowToUse =
 void showinfo(string pathToUproj)
 {
     //TODO
+    printf("%s\n", pathToUproj.c_str());
 }
 
 void build(string pathToUproj)
 {
     //TODO
+    printf("%s\n", pathToUproj.c_str());
 }
 
 void package(string pathToUproj, string pathToPackage)
 {
     //TODO
+    printf("%s\n%s", pathToUproj.c_str(), pathToPackage.c_str());
 }
+
 
 int main(int argc, char* argv[])
 {
-    //TODO
-
-    // if (argc < 3)
-    // {
-    //     showinfo("error");
-    //     return 1;
-    // }
-    //
-    // if (std::strcmp(argv[2], "show-infos") == 0)
-    // {
-    //     showinfo("poulet"); 
-    // }
-
+    if (argc < 3)
+    {
+        printf(HOWTOUSE.c_str());
+        return 1;
+    }
+    
+    if (strcmp(argv[2], "show-infos") == 0){ showinfo("Test show infos"); }
+    else if (strcmp(argv[2], "build") == 0){ build("Test build"); }
+    else if (strcmp(argv[2], "package") == 0 && argc == 4){ package("Test package", argv[3]); }
+    else
+    {
+        printf(HOWTOUSE.c_str());
+        return 1;
+    }
 
     return 0;
 }
