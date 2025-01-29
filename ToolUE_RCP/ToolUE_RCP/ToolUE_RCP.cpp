01abeffbbd7608905cdf47d0fcc994f806ffc903
Leptar <leo.ultimate34@gmail.com>
2025-01-28 19:33:48 -0500

    Update Show-infos : print the project name

--- ToolUE_RCP/ToolUE_RCP/ToolUE_RCP.cpp
+++ ToolUE_RCP/ToolUE_RCP/ToolUE_RCP.cpp
@@ -1,39 +1,53 @@
 #include <cstdio>
 #include <fstream>
-using namespace std;
 #include <string>
+#include "filesystem" 
 #include "json.hpp"
+using namespace std;
 using json = nlohmann::json;
 
-string HOWTOUSE =
+string HOW_TO_USE =
     "How to use ToolUE_RCP :\n"
-    "ToolUE_RCP [CHEMIN DU UPROJECT] show-infos\n"
-    "ToolUE_RCP [CHEMIN DU UPROJECT] build\n"
-    "ToolUE_RCP [CHEMIN DU UPROJECT] package [CHEMIN DU PACKAGE]\n";
+    "ToolUE_RCP [PATH TO UPROJECT] show-infos\n"
+    "ToolUE_RCP [PATH TO UPROJECT] build\n"
+    "ToolUE_RCP [PATH TO UPROJECT] package [PATH TO PACKAGE]\n";
 
 void showinfo(const string& pathToUproj)
 {
     //TODO
-    printf("%s\n", pathToUproj.c_str());
-    
+
+    // try to open file
     ifstream file(pathToUproj);
     if (!file.is_open())
     {
-        printf("Error in opening file\n");
+        printf("Error in opening file. Check if there is no error on the path\n");
+        printf("%s\n", HOW_TO_USE.c_str());
         return;
     }
     json UPROJ_data = json::parse(file);
-    if (UPROJ_data.contains("Modules")) {printf("lezgo");return;}
-    printf("%s\n", UPROJ_data.dump().c_str());
+
+    // Print the project name
+    string projectName;
+    if (!UPROJ_data.contains("Modules"))
+    {
+       filesystem::path p(pathToUproj);
+        projectName = p.stem().string();
+    } else
+    {
+        projectName = UPROJ_data["Modules"][0]["Name"];
+    }
+    printf("Project Name : %s\n", projectName.c_str());
+
+    
 }
 
-void build(string pathToUproj)
+void build(const string& pathToUproj)
 {
     //TODO
     printf("%s\n", pathToUproj.c_str());
 }
 
-void package(string pathToUproj, string pathToPackage)
+void package(const string& pathToUproj, const string& pathToPackage)
 {
     //TODO
     printf("%s\n%s", pathToUproj.c_str(), pathToPackage.c_str());
@@ -44,7 +58,7 @@ int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        printf(HOWTOUSE.c_str());
+        printf(HOW_TO_USE.c_str());
         return 1;
     }
     
@@ -53,7 +67,7 @@ int main(int argc, char* argv[])
     else if (strcmp(argv[2], "package") == 0 && argc == 4){ package(argv[1], argv[3]); }
     else
     {
-        printf(HOWTOUSE.c_str());
+        printf(HOW_TO_USE.c_str());
         return 1;
     }
 
